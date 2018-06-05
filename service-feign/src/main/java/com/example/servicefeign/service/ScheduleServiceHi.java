513ecc58c408dcf823e7c233cffd00fad02e638d
zt1115798334 <zhangtong9498@qq.com>
2018-06-05 15:56:37 +0800

    zt

--- service-feign/src/main/java/com/example/servicefeign/service/ScheduleServiceHi.java
+++ service-feign/src/main/java/com/example/servicefeign/service/ScheduleServiceHi.java
@@ -1,10 +1,12 @@
 package com.example.servicefeign.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Component
 @FeignClient(value = "service-hi")
 public interface ScheduleServiceHi {
 
