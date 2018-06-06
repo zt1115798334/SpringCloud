8a702f7c7e0439c892c8743a4376c564ced772fe
zt1115798334 <zhangtong9498@qq.com>
2018-06-06 16:20:12 +0800

    zt

---
+++ service-hystrix-dashboard/src/main/java/com/example/servicehystrixdashboard/ServiceHystrixDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.example.servicehystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class ServiceHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHystrixDashboardApplication.class, args);
+    }
+}
