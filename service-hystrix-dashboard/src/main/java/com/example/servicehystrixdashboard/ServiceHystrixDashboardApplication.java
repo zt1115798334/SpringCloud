5e6815f4cbd39f47eba50ee749ae6b6327b9fe65
zt1115798334 <zhangtong9498@qq.com>
2018-06-07 13:33:19 +0800

    zt

--- service-hystrix-dashboard/src/main/java/com/example/servicehystrixdashboard/ServiceHystrixDashboardApplication.java
+++ service-hystrix-dashboard/src/main/java/com/example/servicehystrixdashboard/ServiceHystrixDashboardApplication.java
@@ -2,10 +2,12 @@ package com.example.servicehystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceHystrixDashboardApplication {
 
     public static void main(String[] args) {
