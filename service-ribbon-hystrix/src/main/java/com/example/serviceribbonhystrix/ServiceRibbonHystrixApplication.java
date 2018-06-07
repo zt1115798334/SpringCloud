7bebc4deb978bec7287e010bd5625574890bce62
zt1115798334 <zhangtong9498@qq.com>
2018-06-07 14:35:51 +0800

    zt

--- service-ribbon-hystrix/src/main/java/com/example/serviceribbonhystrix/ServiceRibbonHystrixApplication.java
+++ service-ribbon-hystrix/src/main/java/com/example/serviceribbonhystrix/ServiceRibbonHystrixApplication.java
@@ -6,11 +6,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@Configuration
 public class ServiceRibbonHystrixApplication {
 
     public static void main(String[] args) {
